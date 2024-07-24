f178160f9d3041808dc9a30cddf06f3c8548dadc
solarios777 <tsegasolomon635@gmail.com>
2024-07-24 23:22:55 +0300

    deploy on netlify

--- src/API/Axios.js
+++ src/API/Axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/amaclone-8c100/us-central1/api",
+  // localinstance of firebase function
+  // baseURL: "http://127.0.0.1:5001/amaclone-8c100/us-central1/api",
+
+  // deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-bhpi.onrender.com",
 });
